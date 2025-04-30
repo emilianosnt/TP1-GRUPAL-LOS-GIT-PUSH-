90685b7387d94e9381c42a6044e6f2387003b40d
juarezjuan <juarezjuan2012@gmail.com>
2025-04-30 02:11:46 -0300

    punto 5 y 6

---
+++ Punto 5/app.js
@@ -0,0 +1,6 @@
+//Realizado por: Juan Juarez
+
+let num1 = prompt("Ingrese el primer número: ");
+let num2 = prompt("Ingrese el segundo número: ");
+
+alert("La suma de " + num1 + " + " + num2 + " es " + (parseInt(num1) + parseInt(num2)));

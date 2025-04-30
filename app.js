add860f0134563042913a3b2c0d175a487a35a1b
emilianosnt <emilianosant90@gmail.com>
2025-04-30 04:06:42 -0300

    Punto 7

---
+++ app.js
@@ -0,0 +1,8 @@
+//Realizado por: Emiliano Santucho
+
+let num1= prompt("Ingrese el primer número: ");
+let num2= prompt("Ingrese el segundo número: ");
+let num3= prompt("Ingrese el tercer número: ");
+
+numMayor = Math.max(num1, num2, num3);
+alert("El número mayor es: " + numMayor);

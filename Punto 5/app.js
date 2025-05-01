7c4213cf658c58df7587841b6e49ebd0c6b898b0
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2025-05-01 14:04:47 -0300

    correccion de carpetas

--- Punto 5/app.js
+++
@@ -1,6 +0,0 @@
-//Realizado por: Juan Juarez
-
-let num1 = prompt("Ingrese el primer número: ");
-let num2 = prompt("Ingrese el segundo número: ");
-
-alert("La suma de " + num1 + " + " + num2 + " es " + (parseInt(num1) + parseInt(num2)));
